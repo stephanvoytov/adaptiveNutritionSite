e1287812f311f160f1ab810de4e5cfc103490549
stepa323 <voytov.st.vi@gmail.com>
2025-12-03 21:21:40 +0200

    changes for hosting

--- metanit/metanit/settings.py
+++ metanit/metanit/settings.py
@@ -135,6 +135,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
